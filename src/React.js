54c37f44e106179084862e0aa7040aa1aef123df
Markus Andresen <markus.andresen94@gmail.com>
2019-04-02 15:23:45 +0200

    Progress

--- src/React.js
+++ src/React.js
@@ -1,4 +1,4 @@
-//import React from "react"
+// import React from "react"
 
 const flatten = nestedArray => [].concat(...nestedArray)
 
@@ -25,6 +25,7 @@ const _render = (vdom, parent=null) => {
     if (vdom){
         if (vdom.hasOwnProperty("props")){
             if (vdom.props.hasOwnProperty("className")){
+                if (vdom.props.className === "extract-vdom")
                 logVDOM = vdom
             }
         }
@@ -70,8 +71,11 @@ const createElement = (type, props={}, ...children) => {
 }
 
 const _patch = (dom, vdom, parent=dom.parentNode) => {
-    const replace = parent ? el => (parent.replaceChild(el, dom) && el) : (el => el);
-    return replace(_render(vdom, parent))
+    const root = document.getElementById("root")
+    root.removeChild(root.childNodes[0])
+    document.getElementById("root").appendChild(_render(vdom))   
+    //const replace = parent ? el => (parent.replaceChild(el, dom) && el) : (el => el);
+    //return replace(_render(vdom, parent))
 }
 
 
@@ -82,7 +86,7 @@ class Component {
     }
 
 setState(next) {
-    this.state = next
+    this.state = {...this.state, ...next}
     _patch(this.base, this.render());    
 
 }_
--- src/index.js
+++ src/index.js
@@ -5,54 +5,47 @@ const HeadingWithoutJSX = ({ name }) => {
     return React.createElement("h3", { className: "heading"}, name)
 }
 
-const App = () => {
-    const persons = ["Espen", "Corry", "Simen", "Markus"]
-    const list = persons.map((person, index) => {
-        return (<li key={index}>{person}</li>)
-    })
-    return (
-        <div>
-        <HeadingWithoutJSX name="Personer:" />
-        <ul className="myList">
-            {list}
-        </ul>
-    </div>
-    )
-}
-
-class Test extends React.Component {
+class App extends React.Component {
     constructor(props){
         super(props)
         this.state = { 
-            name: "Markus",
+            todos: ["Spise", "Pilse", "Sove"],
             value: ""
         }
         this.props = props || {}
         this.handleChange = this.handleChange.bind(this)
     }
 
-    handleChange(e){  
-        this.setState({ ...this.state, name: e.target.value })
+    handleChange(e){
+        const newTodos = [...this.state.todos]
+        newTodos.push(e.target.value) 
+        this.setState({ todos: newTodos})
     }
   
     render(){
+        const todos = this.state.todos.map((todo, i) => {
+            return (<li key={i} onClick={() => {
+                const filteredTodos = this.state.todos.filter(t => t !== todo)
+                this.setState({todos: filteredTodos})
+            }}> {todo} </li>)
+        })
         return (
-        <div className="extract">
-            Props: {this.props.name}
+        <div className="extract-vdom">
+            <HeadingWithoutJSX name="Todos:" />
+            <ul>
+                {todos}
+            </ul>
             <br />
-            State: {this.state.name}
-            <br />
-            <br />
-            <input type="text" onChange={this.handleChange} />
+            <input placeholder="Type and hit Enter" type="text" onChange={this.handleChange} />
         </div>
     )
     }
 }
     
 
-const vdom = <Test name="Corry" />
+const app = <App name="Corry" />
 
-React.render(vdom, document.getElementById("root"))
+React.render(app, document.getElementById("root"))
 const code = document.getElementById("code")
 code.textContent = JSON.stringify(React.logVDOM(), null, 2);
 
